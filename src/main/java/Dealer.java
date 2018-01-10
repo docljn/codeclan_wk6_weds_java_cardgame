29a3795795bd87cfd683ee1e1973a3bc8b3ce73c
DocLJN <ljnoble@gmail.com>
2018-01-10 16:04:36 +0000

    End of lab time.

--- src/main/java/Dealer.java
+++ src/main/java/Dealer.java
@@ -2,10 +2,17 @@ public class Dealer {
     Deck deck;
     String name;
 
-    public Dealer(String name){
+    public Dealer(String name, Deck deck){
         this.name = name;
         this.deck = deck;
     }
 
 
+    public Deck getDeck() {
+        return this.deck;
+    }
+
+    public dealCard(Player player) {
+
+    }
 }
--- src/test/java/DealerTest.java
+++ src/test/java/DealerTest.java
@@ -1,22 +1,25 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DealerTest {
 
     Deck deck;
     Dealer dealer;
     Player player;
-    Card card;
 
     @Before
     public void before() {
         deck = new Deck();
-        dealer = new Dealer("James");
+        dealer = new Dealer("James", deck);
     }
 
     @Test
     public void canDealCard(){
-        dealer.dealCard(player);
+        dealer.dealCard(deck, player);
+        assertEquals(1, player.getHand().size());
+        assertEquals(51, dealer.getDeck().getCardDeck().size() );
 
 //    }
 }
